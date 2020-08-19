de9b3e5621258bfdc37216fde204d4014f216b19
harshit-labs <61293877+harshit-labs@users.noreply.github.com>
2020-08-19 15:53:22 +0530

    User Profiles 7th commit

---
+++ profiles/api/views.py
@@ -0,0 +1,45 @@
+import random
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
+
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from ..models import Profile
+from ..serializers import PublicProfileSerializer
+
+User = get_user_model()
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def user_profile_detail_view(request, username, *args, **kwargs):
+#     current_user = request.user
+#     to_follow_user = ??
+#     return Response({}, status=200)
+
+@api_view(['GET', 'POST'])
+def profile_detail_api_view(request, username, *args, **kwargs):
+    # get the profile for the passed username
+    qs = Profile.objects.filter(user__username=username)
+    if not qs.exists():
+        return Response({"detail": "User not found"}, status=404)
+    profile_obj = qs.first()
+    data = request.data or {}
+    if request.method == "POST":
+        me = request.user
+        action = data.get("action")
+        if profile_obj.user != me:
+            if action == "follow":
+                profile_obj.followers.add(me)
+            elif action == "unfollow":
+                profile_obj.followers.remove(me)
+            else:
+                pass
+    serializer = PublicProfileSerializer(instance=profile_obj, context={"request": request})
+    return Response(serializer.data, status=200)
---
+++ profiles/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=150, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
+
+def user_did_save(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+    
+post_save.connect(user_did_save, sender=User)
